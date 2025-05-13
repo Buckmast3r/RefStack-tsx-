24ccefdece0fad6272af556ba7d43b65058002ff
pi-swap-test <recording.buck@gmail.com>
2025-05-13 02:37:53 -0600

    init

--- src/lib/stripe/client.ts
+++ src/lib/stripe/client.ts
@@ -1,13 +1,12 @@
-import Stripe from 'stripe'
+import { loadStripe } from '@stripe/stripe-js';
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error('Missing STRIPE_SECRET_KEY')
+const stripePublicKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
+
+if (!stripePublicKey) {
+  throw new Error('Missing NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
-  typescript: true,
-})
+export const stripe = loadStripe(stripePublicKey);
 
 export const PLANS = {
   FREE: {
